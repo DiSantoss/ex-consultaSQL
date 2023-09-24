f2be2cd79f3e30dde842c66f92e04cead3d98145
Di <d.2120leandro@gmail.com>
2023-09-24 01:10:08 -0300

    .2 da lista

---
+++ exercicios-backend-consultas-sql-master/01/query.sql
@@ -0,0 +1,5 @@
+
+--SELECT id,compositor FROM musicas WHERE tempo > 120 AND id BETWEEN 47 AND 123
+
+--SELECT * FROM musicas WHERE compositor IS NOT NULL   AND tempo < 5*60   AND compositor <> 'Bach';
+
